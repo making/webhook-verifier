5788fc95b3fbd093aeffcef19de1f77e9632e781
Toshiaki Maki <tmaki@vmware.com>
2023-03-19 17:49:19 +0900

    Add HandlerInterceptor

---
+++ src/main/java/am/ik/webhook/WebhookHttpHeaders.java
@@ -0,0 +1,9 @@
+package am.ik.webhook;
+
+public class WebhookHttpHeaders {
+
+	public static final String X_HUB_SIGNATURE = "X-Hub-Signature";
+
+	public static final String X_HUB_SIGNATURE_256 = "X-Hub-Signature-256";
+
+}
